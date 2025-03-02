4189859eeaea2bbb8b660bb06c78b8121f7f5dca
dsanchez1120 <40.swampy.lunches@icloud.com>
2025-03-02 00:03:45 -0700

    Added comments to public methods in ElevatorImpl.java

--- src/main/java/com/project/app/elevator/ElevatorImpl.java
+++ src/main/java/com/project/app/elevator/ElevatorImpl.java
@@ -24,6 +24,15 @@ public class ElevatorImpl implements Elevator {
     private int direction;
     private int currentFloor;
 
+    /**
+     * Simulates an elevator button being pressed
+     * If {@code button} corresponds with an item in {@code floors}, that floor is added to {@code floorButtons}
+     * If {@code button} is {@code "open"}, opens elevator door
+     * If {@code} button is {@code "code"}, nothing happens (as with a real elevator)
+     * If {@code} button is {@code "emergency"}, simulates emergency actions (closes doors, stops elevator movement)
+     * Otherwise throws an {@code IllegalArgumentException}
+     * @param button String that corresponds to elevator button
+     */
     @Override
     public void buttonPressed(String button) {
         if (floors.contains(button)) {
@@ -35,11 +44,22 @@ public class ElevatorImpl implements Elevator {
         } else if (button.equals("emergency")) {
             callEmergencyServices();
         } else {
-            // Throwing exception as this indicates an error in the code
             throw new IllegalArgumentException("Invalid argument given");
         }
     }
 
+    /**
+     * Simulates an elevator authentication system.
+     * If elevator has no security system, {@code SecurityType.NONE}, user is redundantly allowed to access all floors
+     * If elevator has general security, {@code SecurityType.GENERAL},
+     *      user is allowed to access all floors if {@code card} is in {@code authorizedUsers}
+     * If elevator has specific security, {@code SecurityType.SPECIFIC},
+     *      user is allowed to access floors in {@code floors} argument if {@code card} is in {@code authorizedUsers}
+     * If elevator security is not {@code SecurityType.NONE}, and {@code card} is not in {@code authorizedUsers}
+     *      user is not allowed to access any floors.
+     * @param card Simulates swiping an RFID card. Contains a string that is checked against {@code authorizedUsers}
+     * @param floors The floors that the user is allowed to access. Structure of array matches {@code floorButtons} global array.
+     */
     @Override
     public void authenticate(String card, ArrayList<Boolean> floors) {
         if (securityType.equals(SecurityType.NONE)) {
@@ -59,32 +79,31 @@ public class ElevatorImpl implements Elevator {
         }
     }
 
-    // FIXME Could be made private
-    // FIXME Simplify Door Status to trinary system
-        // FIXME:  1 = UP
-        // FIXME: -1 = DOWN
-        // FIXME:  0 = STATIONARY
+    /**
+     * Simulates an elevator moving between floors.
+     * Closes elevator doors and according to {@code direction} global variable, recursively chooses next floor based on
+     * {@code floorButtons} global variable. If no floor in that direction has been requested, the opposite direction
+     * will be recursively searched. In either case, if a requested floor is found, {@code currentFloor} global variable
+     * is set to the proper index and elevator doors are opened. If no requested floor is found, elevator moves to
+     * {@code defaultFloor} (global variable).
+     */
     @Override
     public void moveCurrentFloor() {
         closeDoor();
-        int moveDir = -1;
+        // Changes elevator to move UP or DOWN if it is set to stationary.
         if (direction == 0) {
             changeDirection();
-        } else if (direction == 1) {
-            moveDir = 1;
         }
-
-        if (!findNextAvailableFloor(currentFloor, moveDir)) {
-            // Switches direction of elevator and retries
+        if (!findNextAvailableFloor(currentFloor)) {
             changeDirection();
-            if (!findNextAvailableFloor(currentFloor, moveDir * -1)) {
+            if (!findNextAvailableFloor(currentFloor)) {
                 currentFloor = defaultFloor;
                 changeDirection();
             }
         }
     }
-
-    private boolean findNextAvailableFloor(int index, int dir) {
+    // Recursive helper method for moveCurrentFloor
+    private boolean findNextAvailableFloor(int index) {
         if (index == floors.size() || index == -1) {
             return false;
         } else if (floorButtons.get(index)) {
@@ -94,10 +113,11 @@ public class ElevatorImpl implements Elevator {
             return true;
         }
         else {
-            return findNextAvailableFloor(index + dir, dir);
+            return findNextAvailableFloor(index + direction);
         }
     }
 
+    // Called by buttonPressed method. If authorized, requested floor (newFloor) will be added to floorButtons
     private void addFloor(int newFloor) {
         if (checkSecurity(newFloor)) {
             if (currentFloor != newFloor) {
